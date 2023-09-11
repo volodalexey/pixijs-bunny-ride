bb6ec4c5151287c7a5dbe19612cdbd76bcccc422
Alexey <volodalexey@gmail.com>
2023-09-11 20:48:53 +0300

    Corrected webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,9 @@ module.exports = {
     filename: "[name].bundle.js",
     chunkFilename: "[name].chunk.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    clean: {
+      keep: /\.git\//,
+    },
   },
   plugins: [
     new MiniCssExtractPlugin(),
